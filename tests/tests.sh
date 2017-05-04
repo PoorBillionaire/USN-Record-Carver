13b26f65b79d7e78407681e3c4056bd35fe991e7
Adam Witt <accidentalassist@gmail.com>
2017-05-04 14:18:07 -0700

    Fixing test/tests.sh

--- tests/tests.sh
+++ tests/tests.sh
@@ -7,7 +7,7 @@ usncarve.py -f usnjrnl.bin -o /tmp/usn-carved.bin
 usn.py -f /tmp/usn-carved.bin -o /tmp/usn-parsed.txt
 cat /tmp/usn-parsed.txt
 rm /tmp/usn-parsed.txt
-usn.py --csv -f /tmp/carved.bin -o /tmp/usn-parsed.txt
+usn.py --csv -f /tmp/usn-carved.bin -o /tmp/usn-parsed.txt
 cat /tmp/usn-parsed.txt
 rm /tmp/usn-parsed.txt
 usn.py --verbose -f /tmp/usn-carved.bin -o /tmp/usn-parsed.txt
