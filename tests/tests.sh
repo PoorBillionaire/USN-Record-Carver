e8ea082842cd70a0fb293a76bc137da5f0ea4589
Adam Witt <accidentalassist@gmail.com>
2017-05-04 14:11:25 -0700

    Fixing tests/tests.sh

--- tests/tests.sh
+++ tests/tests.sh
@@ -10,15 +10,15 @@ rm /tmp/usn-parsed.txt
 usn.py --csv -f /tmp/carved.bin -o /tmp/usn-parsed.txt
 cat /tmp/usn-parsed.txt
 rm /tmp/usn-parsed.txt
-usn.py --verbose -f usn-carved.bin -o /tmp/usn-parsed.txt
+usn.py --verbose -f /tmp/usn-carved.bin -o /tmp/usn-parsed.txt
 cat /tmp/usn-parsed.txt
 rm /tmp/usn-parsed.txt
-usn.py --tln -f usn-carved.bin -o /tmp/usn-parsed.txt
+usn.py --tln -f /tmp/usn-carved.bin -o /tmp/usn-parsed.txt
 cat /tmp/usn-parsed.txt
 rm /tmp/usn-parsed.txt
-usn.py --tln --system ThisIsASystemName -f usn-carved.bin -o /tmp/usn-parsed.txt
+usn.py --tln --system ThisIsASystemName -f /tmp/usn-carved.bin -o /tmp/usn-parsed.txt
 cat /tmp/usn-parsed.txt
 rm /tmp/usn-parsed.txt
-usn.py --body -f usn-carved.bin -o /tmp/usn-parsed.txt
+usn.py --body -f /tmp/usn-carved.bin -o /tmp/usn-parsed.txt
 cat /tmp/usn-parsed.txt
 mactime -b /tmp/usn-parsed.txt
